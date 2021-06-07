2cd1c1e684c4db62c798054819a8a8595f51543e
adam <adamsyammaszaki@gmail.com>
2021-06-07 13:03:09 +0900

    Median maintenance, bugs fixed

--- Graph Search. Shortest Path, and Data Structure/Week 3/heaptree.cpp
+++ Graph Search. Shortest Path, and Data Structure/Week 3/heaptree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 using namespace std;
 
 typedef struct node_t
@@ -20,7 +21,7 @@ public:
     node *insertplace; //node location to insert new node
     node *leafnode; // the bottom right most node in the tree
     int size = 1; //size of the tree
-    bool min;
+    bool min; //is it a minimum heaptree or a maximum heaptree?
 public:
     heaptree(int i, bool min)
     {
@@ -38,6 +39,7 @@ public:
 
     void insert_node(int key)
     {
+        //create a new node
         size += 1;
         node *p = (struct node_t*)malloc(sizeof(node));
         p->key = key;
@@ -46,6 +48,7 @@ public:
         p->child_l = NULL;
         p->child_r = NULL;
         
+        //shifting pointer to a new node
         if(insertplace->child_l == NULL) 
         {
             insertplace->child_l = p;
@@ -68,6 +71,7 @@ public:
     
     void swapkey(node* a, node* b)
     {
+        /*swap only the key*/
         int t = a->key;
         a->key = b->key;
         b->key = t;
@@ -128,6 +132,7 @@ public:
 
     node* minmax_key(node* a, node* b)
     {
+        /*Choose the approriate children node for extract minmax operation*/
         if(min==true)
         {
             if(a->key < b->key) return a;
@@ -142,13 +147,15 @@ public:
 
     int extract_minmax()
     {
+        /*Extracting (removing) min or max key from heap (depending on the type of the heap)*/
         int res = root->key;
+        node *tparent = leafnode->parent;
         swapkey(root, leafnode);
         size -= 1;
         
         //deleting the current leaf
-        if(leafnode->parent->child_l == leafnode) leafnode->parent->child_l == NULL;
-        else if(leafnode->parent->child_r == leafnode) leafnode->parent->child_r = NULL;
+        if(tparent->child_l == leafnode) tparent->child_l = NULL;   
+        else if(tparent->child_r == leafnode) tparent->child_r = NULL;
 
         //shift leafnode
         leafnode = leafnode->prevorder;
@@ -157,6 +164,13 @@ public:
         bubble_down();
         return res;
     }
+
+    int pass_minmax()
+    {
+        /*Only return the min or max value (this operiation does no remove the minmax value from the heap)*/
+        return root->key;
+    }
+
     void print_heap()
     {
         node *p = root;
@@ -174,6 +188,66 @@ public:
 
 int main()
 {
+    string line;
+    ifstream file;
+    int x;
+    vector<int> data;
+    file.open("data.txt");
+    if(file.is_open())
+    {
+        while(getline(file, line))
+        {
+            x = stoi(line);
+            data.push_back(x);
+        }
+    }
+
+    //initializing heap
+    heaptree H1(data[1], false);
+    heaptree H2(data[0], true);
+
+    int summedian = 0;
+    int median = 0;
+
+    for(int i=0; i<data.size(); i++)
+    {   
+        //the first two values have been initialized
+        if(i==0) median = data[i];
+        else if(i==1) median = H1.pass_minmax();
+        else if(i>1)
+        {
+            cout << data[i];
+            //insert data to the heap according to its root key
+            if(data[i]<H1.root->key) H1.insert_node(data[i]);
+            else H2.insert_node(data[i]);
+
+
+            //check if the heap is not balance, and balance the heap
+            int t;
+            if((H1.size-H2.size)>=2)
+            {
+                t = H1.extract_minmax();
+                H2.insert_node(t);
+            }
+            else if((H2.size-H1.size)>=2)
+            {
+                t = H2.extract_minmax();
+                H1.insert_node(t);
+            }
+        
+            //extracting median
+            if(H1.size==H2.size) median = H1.pass_minmax();
+            else if(H1.size>H2.size) median = H1.pass_minmax();
+            else if(H2.size>H1.size) median = H2.pass_minmax();
+        }
+
+        summedian += median;
+    }
+
+    cout << summedian%10000 << endl;
+
+    /*
+    //for the sake of debugging
     heaptree H(9, true);
     H.insert_node(1);
     H.insert_node(2);
@@ -187,6 +261,19 @@ int main()
     int t = H.extract_minmax();
     cout << "Extracting min " << t << endl;
     H.print_heap();
+
+    cout << endl;
+
+    H2.print_heap();
+
+    cout << endl;
+    H2.insert_node(24);
+    H2.print_heap();
+
+    cout << endl;
+    int t = H2.extract_minmax();
+    H2.print_heap();
+    */
 
 
 
